7d2d184d8b38c80f236d60d093644ffd83c7fac6
fiuzalucas <143010962+fiuzalucas@users.noreply.github.com>
2025-04-04 11:54:36 -0300

    oigis

--- main.js
+++ main.js
@@ -15,9 +15,9 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2025-06-05T00:00:00");
-const tempoObjetivo2 = new Date("2025-05-05T00:00:00");
-const tempoObjetivo3 = new Date("2025-08-30T00:00:00");
+const tempoObjetivo1 = new Date("2025-12-18T00:00:00");
+const tempoObjetivo2 = new Date("2026-08-24T00:00:00");
+const tempoObjetivo3 = new Date("2025-04-26T00:00:00");
 const tempoObjetivo4 = new Date("2025-11-01T00:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
